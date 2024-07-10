489d0405531e1cdd5cf5f8ca95c7d1493a8b9d8d
israelwalden <waldenisraelk@gmail.com>
2024-07-10 08:39:11 +0000

    game api

---
+++ checkers/constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import pygame
+
+WIDTH = 800
+HEIGHT = 800
+
+ROWS = 8
+COLS = 8
+
+SQUARE_SIZE = WIDTH//COLS
+
+RED =(255,0,0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREY = (128,128,128)
